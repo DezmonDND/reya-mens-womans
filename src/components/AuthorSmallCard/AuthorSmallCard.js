204f28438294ef45e6b06fed05b781651b643679
unknown <denedoseikin@mail.ru>
2024-10-01 00:12:28 +0700

    feat: добавить поиск при клике по карточке

--- src/components/AuthorSmallCard/AuthorSmallCard.js
+++ src/components/AuthorSmallCard/AuthorSmallCard.js
@@ -1,24 +1,27 @@
 import "./AuthorSmallCard.css";
-import WithoutAvatarIcon from '../../images/no_avatar_icon.svg'
+import WithoutAvatarIcon from "../../images/no_avatar_icon.svg";
 
 function AuthorSmallCard(props) {
-  const { author } = props;
+  const { author, handleAuthorClick } = props;
 
   return (
-    <div className="authors-short__content">
-      <div type="input" className="authors-short__button">
+    <button
+      className="authors-short__content"
+      onClick={() => handleAuthorClick(author)}
+    >
+      <div className="authors-short__button">
         <img
           className="authors-short__photo"
-          src={author.avatar !== '' ? author.avatar : WithoutAvatarIcon}
+          src={author.avatar !== "" ? author.avatar : WithoutAvatarIcon}
           alt={`Фотография ${author.name}`}
         ></img>
       </div>
       <div className="authors-short__info">
-        <h2 className="authors-short__name">{author.name}</h2>
+        <div className="authors-short__name">{author.name}</div>
         <p className="authors-short__job">{author.job}</p>
         <p className="authors-short__education">{author.education}</p>
       </div>
-    </div>
+    </button>
   );
 }
 
